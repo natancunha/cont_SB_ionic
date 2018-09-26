fabe81cbc76ecb7541eff80f93910cfa86fff920
Natan <natancunha@gmail.com>
2018-09-26 15:13:17 -0300

    initial commit

---
+++ src/main/java/com/natan/cursosb/CursosbApplication.java
@@ -0,0 +1,12 @@
+package com.natan.cursosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosbApplication.class, args);
+	}
+}
