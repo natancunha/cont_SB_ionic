02b821351e3baed3b39e6ba9988070bd53047d3b
Natan <natancunha@gmail.com>
2018-09-28 17:29:54 -0300

    criado classe produto e feita associação many2many

--- src/main/java/com/natan/cursosb/CursosbApplication.java
+++ src/main/java/com/natan/cursosb/CursosbApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.natan.cursosb.domain.Categoria;
+import com.natan.cursosb.domain.Produto;
 import com.natan.cursosb.repositories.CategoriaRepository;
+import com.natan.cursosb.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursosbApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
@@ -24,10 +29,21 @@ public class CursosbApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");	
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 }
