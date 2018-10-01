f976e1ca3043f5a326a8fe3585dc3ed4adce7ce1
Natan <natancunha@gmail.com>
2018-10-01 16:53:18 -0300

    cidade e estado

--- src/main/java/com/natan/cursosb/CursosbApplication.java
+++ src/main/java/com/natan/cursosb/CursosbApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.natan.cursosb.domain.Categoria;
+import com.natan.cursosb.domain.Cidade;
+import com.natan.cursosb.domain.Estado;
 import com.natan.cursosb.domain.Produto;
 import com.natan.cursosb.repositories.CategoriaRepository;
+import com.natan.cursosb.repositories.CidadeRepository;
+import com.natan.cursosb.repositories.EstadoRepository;
 import com.natan.cursosb.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursosbApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
@@ -42,8 +52,21 @@ public class CursosbApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Cidade cid1 = new Cidade(null, "Uberlandia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
 	}
 }
